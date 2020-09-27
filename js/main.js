12a875a3bb5439b706efc36a031fd79e48f977c0
Taha Kassar <tahakassar54@gmail.com>
2020-09-27 16:13:13 +0300

    Sesdd

--- js/main.js
+++ js/main.js
@@ -93,3 +93,17 @@ $(document).ready(function() {
     });
 	
 });
+
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "<sender’s email address>",
+	Password : "<email password>",
+	To : 'tahakassar54@gmail.com',
+	From : "<sender’s email address>",
+	Subject : "<email subject>",
+	Body : "<email body>",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
